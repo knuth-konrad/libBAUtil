c19762f23421c0637d99f3927ec529be34671b94
Knuth Konrad <Knuth.Konrad@BasicAware.de>
2021-03-17 11:36:04 +0100

    FIX:FilesystemUtils.BackupFile() never created a valid file name.

--- CLS/FilesystemUtil.vb
+++ CLS/FilesystemUtil.vb
@@ -172,10 +172,10 @@ Public Class FilesystemUtil
          If incrementTarget = True Then
             i = 0
             ' Generate a new file name that eventually doesn't exist in the target folder. Stop at 9999!
-            tempFile = NormalizePath(destPath) & destFile & "." & String.Format("{0}:0000", i) & destExt
+            tempFile = NormalizePath(destPath) & destFile & "." & String.Format("{0:0000}", i) & destExt
             Do While Exists(tempFile) = True And i < 9999
                i += 1
-               tempFile = NormalizePath(destPath) & destFile & "." & String.Format("{0}:0000", i) & destExt
+               tempFile = NormalizePath(destPath) & destFile & "." & String.Format("{0:0000}", i) & destExt
             Loop
          Else
             tempFile = fileDest
